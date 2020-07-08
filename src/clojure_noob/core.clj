21de6e67ed96bdfcc0471f9aaae695c25efaa1e6
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2020-07-08 21:38:41 +0200

    sequence abstraction

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -6,6 +6,7 @@
   [& args]
   (println "I'm a little teapot!"))
 
+; ---------------------------------- Hobbit
 (def asym-hobbit-body-parts [{:name "head" :size 3}
                              {:name "left-eye" :size 1}
                              {:name "left-ear" :size 1}
@@ -22,9 +23,7 @@
                              {:name "left-hand" :size 2}
                              {:name "left-knee" :size 2}
                              {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}])
+                             {:name "left-lower-leg" :size 3}])
 
 (defn matching-part
   [part]
@@ -61,3 +60,112 @@
         part
         (recur remaining (+ accumulated-size (:size (first remaining))))))
     ))
+; ----------------------------------------------------------
+; ------------- Vampires
+
+(def human-consumption [8.1 7.3 6.6 5.0])
+(def critter-consumption [0.0 0.2 0.3 1.1])
+(defn unify-diet-data
+  [human critter]
+  {:human human
+   :critter critter})
+
+(def multi-collection (map unify-diet-data human-consumption critter-consumption))
+
+(def sum #(reduce + %))
+(def avg #(/ (sum %) (count %)))
+
+(defn stats
+  [numbers]
+  (map #(% numbers) [sum count avg]))
+
+(def identities
+  [{:alias "Batman" :real "Bruce Wayne"}
+   {:alias "Spider-Man" :real "Peter Parker"}
+   {:alias "Santa" :real "Your mom"}
+   {:alias "Easter Bunny" :real "Your dad"}])
+
+(def reals  (map :real identities))
+
+
+(def initial-map {:max 30 :min 10})
+
+(defn update-map
+  [my-map]
+  (reduce (fn [new-map [key val]]
+            (assoc new-map key (inc val))) {} my-map))
+
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9})
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 4)
+     (drop-while #(< (:month %) 2) food-journal))
+
+(defn is-containing-critter-more-than-3
+    []
+ (some #(> (:critter %) 3) food-journal) )
+
+(defn get-critter-more-than-3
+  []
+  (some #(and (> (:critter %) 3) %) food-journal))
+
+(sort-by count ["aaa" "c" "bb"])
+
+(sort ["aaa" "c" "bb"])
+
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+
+(concat (take 8 (repeat "na")) ["Batman!"])
+
+(take 3 (repeatedly (fn [] (rand-int 10))))
+
+(defn even-numbers
+  ([] (even-numbers 0))
+  ; use lazy-seq in order to evaluate (in this case make the recursion) only when need and prevent infinite-loop
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+
+
+(def the-map
+  (into {} (map identity {:sunlight-reaction "Glitter!"})))
+
+(def the-set
+  (into #{} (map identity [:garlic-love :garlic-love])))
+; --------
+
